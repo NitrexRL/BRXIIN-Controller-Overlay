82fc8cb866416a1e49d71dbf546e9b02c5d39ac4
Nico Berger <nico.berger.904@gmail.com>
2026-06-05 03:06:12 +0200

    Vite config korrigiert

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 
-// Set the base path to the repository name for GitHub Pages deployment.
-// This ensures assets resolve correctly when served from https://<user>.github.io/ps5-controller-visualizer/
 export default defineConfig({
-  base: base: '/BRXIIN-Controller-Overlay/'
+  base: '/BRXIIN-Controller-Overlay/',
   server: {
     host: true,
     open: true,
